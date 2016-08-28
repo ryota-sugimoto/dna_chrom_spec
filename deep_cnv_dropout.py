ccc5156203cf282d245c2802db16073b3a695c3a
Ryota Sugimoto <ryota.sugimoto@gmail.com>
2016-08-29 00:13:29 +0900

    added more layers

--- deep_cnv_dropout.py
+++ deep_cnv_dropout.py
@@ -9,7 +9,6 @@ from keras.utils import np_utils
 data = scipy.io.loadmat("data.mat")
 
 all_size = data["seq_tr"].shape[0]
-train_size = 54000
 
 comp_mat = np.matrix([[0,0,0,1],
                       [0,0,1,0],
@@ -25,43 +24,31 @@ def reverse_complement(seqs):
     l.append(a)
   return np.array(l)
 
-def reverse(seqs):
-  l = []
-  for seq in seqs:
-    a = seq[::-1]
-    l.append(a)
-  return np.array(l)
+import numpy.random
+def shuffle_together(a, b):
+    assert len(a) == len(b)
+    shuffled_a = numpy.empty(a.shape, dtype=a.dtype)
+    shuffled_b = numpy.empty(b.shape, dtype=b.dtype)
+    permutation = numpy.random.permutation(len(a))
+    for old_index, new_index in enumerate(permutation):
+        shuffled_a[new_index] = a[old_index]
+        shuffled_b[new_index] = b[old_index]
+    return shuffled_a, shuffled_b
 
-def complement(seqs):
-  l = []
-  for seq in seqs:
-    a = np.array(seq*comp_mat)
-    l.append(a)
-  return np.array(l)
-
-out_n = 3
-X_train = data["seq_tr"][:train_size].reshape(train_size,200,4)
-Y_train = data["out_tr"][:train_size]
+sample_size = 60000
+X_train = data["seq_tr"].reshape(sample_size,200,4)
+Y_train = data["out_tr"]
 X_train_rc = reverse_complement(X_train)
-X_train_r = reverse(X_train)
-X_train_c = complement(X_train)
 X_train = np.concatenate([X_train,X_train_rc])
 Y_train = np.concatenate([Y_train]*2)
-#Y_train = Y_train[:,out_n]
 
-
-X_test = data["seq_tr"][train_size:].reshape(all_size-train_size,200,4)
-Y_test = data["out_tr"][train_size:]
-#Y_test = Y_test[:,out_n]
+X_train,Y_train = shuffle_together(X_train, Y_train)
 
 X_practice = data["seq_te"].reshape(10000,200,4)
 
-import theano
-theano.config.floatX = "float32"
-X_train = X_train.astype(theano.config.floatX)
-Y_train = Y_train.astype(theano.config.floatX)
-X_test = X_test.astype(theano.config.floatX)
-Y_test = Y_test.astype(theano.config.floatX)
+X_train = X_train.astype(dtype=np.float32)
+Y_train = Y_train.astype(dtype=np.float32)
+X_practice = X_practice.astype(dtype=np.float32)
 
 from keras.models import Sequential
 from keras.layers.core import Dense,Flatten,Dropout
@@ -98,6 +85,20 @@ model.add(Convolution1D(64, 3,
                         activation="relu"))
 model.add(BN())
 model.add(Dropout(0.5))
+model.add(Convolution1D(64, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+model.add(Convolution1D(64, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+
+
 
 #conv_2
 model.add(Convolution1D(128, 3,
@@ -113,6 +114,20 @@ model.add(Convolution1D(128, 3,
                         activation="relu"))
 model.add(BN())
 model.add(Dropout(0.5))
+model.add(Convolution1D(128, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+model.add(Convolution1D(128, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+
+
 
 #conv_3
 model.add(Convolution1D(256, 3,
@@ -128,8 +143,48 @@ model.add(Convolution1D(256, 3,
                         activation="relu"))
 model.add(BN())
 model.add(Dropout(0.5))
-model.add(Flatten())
+model.add(Convolution1D(256, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+model.add(Convolution1D(256, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
 
+
+
+#conv_4
+model.add(Convolution1D(512, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        subsample_length=2,
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+model.add(Convolution1D(512, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+model.add(Convolution1D(512, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+model.add(Convolution1D(512, 3,
+                        border_mode="same",
+                        W_constraint=maxnorm(2),
+                        activation="relu"))
+model.add(BN())
+model.add(Dropout(0.5))
+model.add(Flatten())
 
 #fc
 model.add(Dense(2048,
@@ -150,43 +205,41 @@ model.add(Dense(8,
                 activation="sigmoid"))
 
 
+from keras.optimizers import Adam,SGD
 model.compile(loss="binary_crossentropy",
               optimizer="adam",
               metrics=["accuracy","mae"])
-'''
-for i,layer in enumerate(model.layers):
-  print i,"input",layer.input
-  print i,"output",layer.output
-  print
-exit(0)
-'''
 
 from numpy import vstack
 from sklearn.metrics import roc_auc_score
 import sys
 
-per_epoch = 100
+per_epoch = 1
+validation_ratio = 0.05
+train_size = int(sample_size*2*(1-validation_ratio))
 
-for i in range(10):
+for i in range(1):
   model.fit(X_train,
             Y_train,
             shuffle=True,
             nb_epoch=per_epoch,
             batch_size=500,
-            verbose=2,
-            validation_split=0.1)
+            verbose=1,
+            validation_split=validation_ratio)
   
 
   Y_train_pred = model.predict_proba(X_train, verbose=0)
-  Y_test_pred = model.predict_proba(X_test, verbose=0)
-  train_auc,test_auc = [],[]
+  train_auc = []
+  validation_auc = []
   for j in range(8):
-    train_auc.append(roc_auc_score(Y_train[:,j], Y_train_pred[:,j]))
-    test_auc.append(roc_auc_score(Y_test[:,j], Y_test_pred[:,j]))
+    train_auc.append(roc_auc_score(Y_train[:train_size,j],
+                                   Y_train_pred[:train_size,j]))
+    validation_auc.append(roc_auc_score(Y_train[train_size:,j],
+                                        Y_train_pred[train_size:,j]))
   print "train_auc", " ".join(map(lambda f: "%0.03f"%(f,), train_auc))
   print "mean_train_auc", sum(train_auc)/float(len(train_auc))
-  print "test_auc", " ".join(map(lambda f: "%0.03f"%(f,), test_auc))
-  print "mean_test_auc",  sum(test_auc)/float(len(test_auc))
+  print "validation_auc", " ".join(map(lambda f: "%0.03f"%(f,), validation_auc))
+  print "mean_validation_auc", sum(validation_auc)/float(len(validation_auc))
 
   model.save(str((i+1)*per_epoch)+"_epoch_"+"model.hdf")
 
